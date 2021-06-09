14c8555f515d27f3f7e8180a750bd396b47f4471
CaoiveMcTigue <keymctigue92@gmail.com>
2021-06-09 22:40:44 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,27 @@ document.addEventListener ("DOMContentLoaded", function() {
                 alert("You clicked submit!");
             } else{
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+    runGame("addition");
 })
 
-function runGame() {
 
+/**
+ * The main game loop.
+ */
+function runGame(gameType) {
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if(gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else{
+        alert(`Unkown game type: ${gameType}`);
+        throw `Unkown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -32,8 +45,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+        document.getElementById('operand1').textContent = operand1;
+        document.getElementById('operand2').textContent = operand2;
+        document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
