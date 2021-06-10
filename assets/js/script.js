9c4bbb8abcd7437c81e044ed8ab42c1b6bc56ed7
CaoiveMcTigue <keymctigue92@gmail.com>
2021-06-10 17:22:19 +0000

    Add final tidy ups for users

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,13 @@ document.addEventListener ("DOMContentLoaded", function() {
             }
         })
     }
+
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if(event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
 })
 
@@ -18,6 +25,10 @@ document.addEventListener ("DOMContentLoaded", function() {
  * The main game loop.
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
